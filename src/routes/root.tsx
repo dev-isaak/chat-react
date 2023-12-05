4d4602a5ef4d08b78bc513f031e8276eca2e1c88
isaac <isaak_1992@hotmail.es>
2023-12-05 18:49:55 +0100

    login

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../features/navbar/Navbar';
+import './rootStyles.css';
 
 export default function Root() {
 	return (
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,7 +1,69 @@
+import TextField from '@mui/material/TextField';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import './assets/LoginViewStyles.css';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 export default function LoginView() {
+	const [showPassword, setShowPassword] = useState(false);
+	const [identification, setIdentification] = useState('');
+	const [password, setPassword] = useState('');
+	const [isErrorInIdentificationField, setIsErrorInIdentificationField] =
+		useState(false);
+	const [isErrorInPasswordField, setIsErrorInPasswordField] = useState(false);
+
+	const handleIdentification = (e: ChangeEvent<HTMLInputElement>) => {
+		setIdentification(e.target.value);
+	};
+
+	const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.target.value);
+	};
+
+	const handleClickShowPassword = () => {
+		setShowPassword(!showPassword);
+	};
+
+	const handleLogin = () => {
+		alert(`email ${identification}, password: ${password}`);
+	};
+
 	return (
 		<div>
-			<h1>Esto es el login</h1>
+			<h1>Login</h1>
+			<form onSubmit={handleLogin} className="login-form">
+				<TextField
+					error={isErrorInIdentificationField ? true : false}
+					helperText={isErrorInIdentificationField ? 'Error in field' : ''}
+					required
+					label="Email"
+					value={identification}
+					onChange={handleIdentification}
+					margin="normal"
+				/>
+				<OutlinedInput
+					error={isErrorInPasswordField ? true : false}
+					required
+					label="Password"
+					value={password}
+					onChange={handlePassword}
+					type={showPassword ? 'text' : 'password'}
+					endAdornment={
+						<InputAdornment position="end">
+							<IconButton onClick={handleClickShowPassword}>
+								{showPassword ? <VisibilityOff /> : <Visibility />}
+							</IconButton>
+						</InputAdornment>
+					}
+				/>
+				<Button variant="contained" type="submit">
+					Login
+				</Button>
+			</form>
 		</div>
 	);
 }
